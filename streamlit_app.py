756b856f2c3ef48c85c94af3247c64bf1be87d36
soumya/b/ghosh <soumyab.g@gmail.com>
2026-02-23 15:28:29 +0000

    st.line_chart(df)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Tirol")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
@@ -76,3 +76,5 @@ plt.tight_layout()
 
 # Display the plot
 st.pyplot(plt.show())
+
+st.line_chart(df)
