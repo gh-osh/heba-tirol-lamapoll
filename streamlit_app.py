c2a00d72e3c2033715b71ded1b2fc3e5f2dd8fcd
soumya/b/ghosh <soumyab.g@gmail.com>
2026-02-25 11:37:19 +0000

    Enhance metrics display in Streamlit app: add total invited participants and percentage deltas for visitors, started, and finished participants

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,41 @@ try:
     response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
     data = response.json()
     #print(json.dumps(data, indent=2))
-    st.success('API request successful. Data retrieved and parsed as JSON.')
+    #st.success('API request successful. Data retrieved and parsed as JSON.')
 except requests.exceptions.RequestException as e:
     st.write(f"Error making API request: {e}")
 except json.JSONDecodeError:
     st.write(f"Error decoding JSON response. Response content: {response.text}")
+
+url_mailing = 'https://app.lamapoll.de/api/v2/polls/1965090/mailings'
+
+headers_mailing = {
+    'accept': 'application/json',
+    'Authorization': 'Bearer '+str(lama_api_key)
+}
+params_mailing = {
+    'limit': 50,
+    'offset': 0,
+    'status': 'done'
+}
+try:
+    response_mailing = requests.get(url_mailing, headers=headers_mailing, params=params_mailing)
+    response_mailing.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+    data_mailing = response_mailing.json()
+    #print(json.dumps(data_mailing, indent=2))
+    #st.success('API request for mailings successful. Data retrieved and parsed as JSON.')
+except requests.exceptions.RequestException as e:
+    st.write(f"Error making API request for mailings: {e}")
+except json.JSONDecodeError:
+    st.write(f"Error decoding JSON response for mailings. Response content: {response_mailing.text}")
+df_mailing = pd.DataFrame(data_mailing)
+
+#df_mailing = df_mailing[df_mailing['attributes'].str.contains('Tirol|TIROL', case=False, na=False)]
+# Convert attributes to string first, then filter
+df_mailing['attributes_str'] = df_mailing['attributes'].astype(str)
+df_mailing = df_mailing[df_mailing['attributes_str'].str.contains('Tirol|TIROL', case=False, na=False)]
+df_mailing = df_mailing.drop('attributes_str', axis=1)  # Remove the temporary column if not needed
+df_mailing_invited = df_mailing['numOfReceivers'].sum()
 
 
 dates = []
@@ -59,11 +89,11 @@ visitors_total = df['Visitors'].sum()
 started_total = df['Started'].sum()
 finished_total = df['Finished'].sum()
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Total Visitors", visitors_total)
+col0, col1, col2, col3 = st.columns(4)
+col0.metric("Total invited", df_mailing_invited)
+col1.metric("Total Visitors", visitors_total, delta=str(round((visitors_total/df_mailing_invited)*100, 2))+"% of invited", delta_arrow="off")
 col2.metric("Total Started", started_total, delta=str(round((started_total/visitors_total)*100, 2))+"% of visitors", delta_arrow="off")
 col3.metric("Total Finished", finished_total, delta=str(round((finished_total/started_total)*100, 2))+"% of started", delta_arrow="off")
-
 
 #st.success("DataFrame created successfully with 'Date' as index.")
 #df.head()
